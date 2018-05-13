6d05bb85f095f1b6c3c0be076891404c48fe7431
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-05-13 07:07:50 +0600

    Waterfall

--- waterfall_arr.js
+++ waterfall_arr.js
@@ -7,23 +7,19 @@ const func1 = (cb) => {
   }, 300);
 };
 
-const func2 = (cb) => {
+const func2 = (res, cb) => {
   setTimeout(() => {
+    console.log(`inside func2 - ${res}`); // func1
     cb(null, 'func2');
-  }, 200);
-};
-
-const func3 = (cb) => {
-  setTimeout(() => {
-    cb(null, 'func3');
   }, 100);
 };
 
 stack.push(func1);
 stack.push(func2);
-stack.push(func3);
 
 
-async.parallel(stack, (err, results) => {
-  console.log(results);
+async.waterfall(stack, (err, res) => {
+  console.log(`waterfall res: ${res}`); // func2
 });
+
+
